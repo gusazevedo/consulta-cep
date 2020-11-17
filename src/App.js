a716c0eda65f17f66b83b811d1773204ea53d269
Gustavo Azevedo <gustavonovaisgan@gmail.com>
2020-11-17 00:04:49 -0300

    estilizando página

--- src/App.js
+++ src/App.js
@@ -24,10 +24,10 @@ function App() {
     fetch(`https://viacep.com.br/ws/${cep}/json/`)
       .then((res) => res.json())
       .then((data) => {
-        setFieldValue('logradouro', data.logradouro);
-        setFieldValue('bairro', data.bairro);
-        setFieldValue('cidade', data.localidade);
-        setFieldValue('uf', data.uf);
+        setFieldValue("logradouro", data.logradouro);
+        setFieldValue("bairro", data.bairro);
+        setFieldValue("cidade", data.localidade);
+        setFieldValue("uf", data.uf);
       });
   }
 
@@ -50,7 +50,11 @@ function App() {
             <h2>Verificar CEP</h2>
             <div className="form-control-group">
               <label>CEP:</label>
-              <Field name="cep" type="text" onBlur={(e) => onBlurCep(e, setFieldValue)} />
+              <Field
+                name="cep"
+                type="text"
+                onBlur={(e) => onBlurCep(e, setFieldValue)}
+              />
             </div>
             <div className="form-control-group">
               <label>Logradouro:</label>
@@ -74,10 +78,39 @@ function App() {
             </div>
             <div className="form-control-group">
               <label>Estado:</label>
-              <Field name="uf" type="text" />
+              <Field component="select" name="uf">
+                <option value={null}>Selecione um estado</option>
+                <option value="AC">Acre</option>
+                <option value="AL">Alagoas</option>
+                <option value="AP">Amapá</option>
+                <option value="AM">Amazonas</option>
+                <option value="BA">Bahia</option>
+                <option value="CE">Ceará</option>
+                <option value="DF">Distrito Federal</option>
+                <option value="ES">Espírito Santo</option>
+                <option value="GO">Goiás</option>
+                <option value="MA">Maranhão</option>
+                <option value="MT">Mato Grosso</option>
+                <option value="MS">Mato Grosso do Sul</option>
+                <option value="MG">Minas Gerais</option>
+                <option value="PA">Pará</option>
+                <option value="PB">Paraíba</option>
+                <option value="PR">Paraná</option>
+                <option value="PE">Pernambuco</option>
+                <option value="PI">Piauí</option>
+                <option value="RJ">Rio de Janeiro</option>
+                <option value="RN">Rio Grande do Norte</option>
+                <option value="RS">Rio Grande do Sul</option>
+                <option value="RO">Rondônia</option>
+                <option value="RR">Roraima</option>
+                <option value="SC">Santa Catarina</option>
+                <option value="SP">São Paulo</option>
+                <option value="SE">Sergipe</option>
+                <option value="TO">Tocantins</option>
+              </Field>
             </div>
 
-            <button type="submit" disabled={!isValid}>
+            <button className="btn" type="submit" disabled={!isValid}>
               Enviar
             </button>
           </Form>
