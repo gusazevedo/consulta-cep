e70da6ddf454a7105648355ee6422f3368d84162
Gustavo Azevedo <gustavonovaisgan@gmail.com>
2020-11-16 22:50:04 -0300

    corrigindo as initialValues e fazendo um regex no campo de cep para que este só aceite números

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Formik, Field, Form, ErrorMessage } from "formik";
+import { Formik, Field, Form } from "formik";
 import Schema from "./schema";
 import "./App.css";
 
@@ -8,24 +8,70 @@ function App() {
     console.log("SUBMIT", values);
   }
 
+  function onBlurCep(e) {
+    const { value } = e.target
+
+    // o regex só vai ler tudo que estiver de 0-9
+    const cep = value.replace(/[^0-9]/g, '');
+    
+    // O "?" é um optional chaining.
+    // Se isso existe (lenght) quero acessar o valor de lenght
+    if(value?.length !== 8) {
+      return;
+    }
+
+    // a alternativa para o fetch seria o axios
+    fetch(`https://viacep.com.br/ws/${value}/json/`)
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }
+
   return (
     <div className="App">
       <Formik
         validationSchema={Schema}
         onSubmit={onSubmit}
-        render={({ values, isValid }) => (
-          <Form>  
-          <h2>Formulário de validação</h2>
-            <div>
-              <label>Name: </label>
-              <Field type="text" name="name" />
-              <ErrorMessage name="name" />
+        initialValues={{
+          cep: "",
+          logradouro: "",
+          numero: "",
+          complemento: "",
+          bairro: "",
+          cidade: "",
+          uf: "",
+        }}
+        render={({ isValid }) => (
+          <Form>
+            <h2>Verificar CEP</h2>
+            <div className="form-control-group">
+              <label>CEP:</label>
+              <Field name="cep" type="text" onBlur={onBlurCep}/>
             </div>
-            <div>
-              <label>Email: </label>
-              <Field type="text" name="email" />
-              <ErrorMessage name="email" />
+            <div className="form-control-group">
+              <label>Logradouro:</label>
+              <Field name="logradouro" type="text" />
             </div>
+            <div className="form-control-group">
+              <label>Número:</label>
+              <Field name="numero" type="text" />
+            </div>
+            <div className="form-control-group">
+              <label>Complemento:</label>
+              <Field name="complemento" type="text" />
+            </div>
+            <div className="form-control-group">
+              <label>Bairro:</label>
+              <Field name="bairro" type="text" />
+            </div>
+            <div className="form-control-group">
+              <label>Cidade:</label>
+              <Field name="cidade" type="text" />
+            </div>
+            <div className="form-control-group">
+              <label>Estado:</label>
+              <Field name="uf" type="text" />
+            </div>
+
             <button type="submit" disabled={!isValid}>
               Enviar
             </button>
--- src/schema.js
+++ src/schema.js
@@ -1,6 +1,5 @@
 import * as Yup from 'yup';
 
 export default Yup.object().shape({
-    name: Yup.string().min(2).required(),
-    email: Yup.string().email().required(),
+    cep: Yup.string().min(2).required(),
 });
